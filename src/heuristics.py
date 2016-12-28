8680e700b27c31b21e412aafb2c4392c69901410
msobiesz-a <msobiesz-a@lion-wave.com>
2016-12-28 12:51:39 +0100

    Add selection implementation

--- src/heuristics.py
+++ src/heuristics.py
@@ -1,17 +1,20 @@
 from raport_generator import RaportGenerator
 from structures import NetworkTree, LineSegment
 from utils import nrst_pt_on_seg
-import random
+from random import sample
+from random import random
+from bisect import bisect_left
 
 class Heuristics:
 
     population = []
     is_population_sorted = False
 
-    def __init__(self, cities_coords, powers_coords, pop_quantity, iter_quantity, cost_traction, cost_power_lines):
+    def __init__(self, cities_coords, powers_coords, pop_quantity, sel_size, iter_quantity, cost_traction, cost_power_lines):
         self.cities_coords = cities_coords
         self.powers_coords = powers_coords
         self.pop_quantity = pop_quantity
+        self.sel_size = sel_size
         self.iter_quantity = iter_quantity
         self.cost_traction = cost_traction
         self.cost_power_lines = cost_power_lines
@@ -27,7 +30,7 @@ class Heuristics:
             #generate end raport
 
     def do_heuristic_iteration(self):
-        self.selection()
+        self.selection(self.sel_size)
         self.crossover()
         self.mutation()
         self.succession()
@@ -48,7 +51,7 @@ class Heuristics:
 
             while len(unusedSet) != 0:
 
-                point = random.sample(unusedSet, 1)
+                point = sample(unusedSet, 1)
                 point = point.pop(0)
                 unusedSet.remove(point)
 
@@ -56,7 +59,7 @@ class Heuristics:
                     usedSet.add(point)
 
                 else:
-                    usedPoint = random.sample(usedSet,1)
+                    usedPoint = sample(usedSet,1)
                     usedPoint = usedPoint.pop(0)
                     individual.add_new_segment(LineSegment(point, usedPoint))
                     usedSet.add(point)
@@ -84,9 +87,23 @@ class Heuristics:
 
             self.population.append(individual)
 
+    def selection(self, size):
+        acc_goal_funcs = list()
+        last_goal_func = 0
+        for individual in self.population:
+            last_goal_func = individual.goal_func + last_goal_func
+            acc_goal_funcs.append(last_goal_func)
+        sel_individuals = list()
+        sel_pos = None
+        for i in range(size):
+            rand_goal_func = random() * acc_goal_funcs[-1]
+            if rand_goal_func in acc_goal_funcs:
+                sel_pos = acc_goal_funcs.index(rand_goal_func)
+            else:
+                sel_pos = bisect_left(acc_goal_funcs, rand_goal_func)
+            sel_individuals.append(self.population[sel_pos])
 
-    def selection(self):
-        print "Selection"
+        return sel_individuals
 
     def crossover(self):
         print "Crossover"
--- src/main.py
+++ src/main.py
@@ -17,9 +17,14 @@ from heuristics import Heuristics
 powers_coord = DataReader().read_locations("../res/powerstations-locations.txt")
 cities_coord = DataReader().read_locations("../res/cities-locations.txt")
 #args = dr.DataReader.read_arguments()
-args = [20, 20, 1.2, 0.7]
+args = [20, 2, 20, 1.2, 0.7]
+# args[0] - initial population size
+# args[1] - selection size
+# args[2] - number of iterations
+# args[3] - cost_traction
+# args[4] - cost_power_lines
 
 
-heur_alg = Heuristics(cities_coord, powers_coord, args[0], args[1], args[2], args[3])
+heur_alg = Heuristics(cities_coord, powers_coord, args[0], args[1], args[2], args[3], args[4])
 heur_alg.run_heuristics()
 
